a26c8e3df79d6beb74119f11ba6d31018fa08837
Pavel Slatvitskii <pavel.slatvitskiy@gmail.com>
2023-12-05 11:09:37 +0600

    add get method

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let app = express();
 
-console.log('Hello World');
+function handler(req, res) {
+  res.send('Hello Express');
+}
+
+app.get('/', handler);
 
 module.exports = app;
