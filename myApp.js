d8c5ee43a6eb50daee42c966a3184861b8c6675c
Pavel Slatvitskii <pavel.slatvitskiy@gmail.com>
2023-12-05 13:08:53 +0600

    add static path

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let app = express();
 
+app.use(express.static(__dirname + '/public/'));
+app.use('/public', express.static(__dirname + '/public'));
+
 function handler(req, res) {
   const path = __dirname + '/views/index.html';
   res.sendFile(path);
