8873b6ff0a96cc9bf9a2c907df097055e9081f74
Pavel Slatvitskii <pavel.slatvitskiy@gmail.com>
2023-12-05 15:42:11 +0600

    add middleware

--- myApp.js
+++ myApp.js
@@ -7,6 +7,11 @@ let app = express();
 
 app.use('/public', express.static(__dirname + '/public'));
 
+function middleware(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+}
+
 function handler(req, res) {
   const path = __dirname + '/views/index.html';
   res.sendFile(path);
@@ -19,7 +24,7 @@ function jsonHandler(req, res) {
     res.json({ message: 'Hello json' });
   }
 }
-
+app.use(middleware);
 app.get('/', handler);
 app.get('/json', jsonHandler);
 
